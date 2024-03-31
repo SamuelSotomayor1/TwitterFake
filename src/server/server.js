466a849470a23d285c8feb6be0f526423076aa46
SamuelSotomayor1 <andressotomayor477@gmail.com>
2024-03-31 00:51:38 -0300

    Realizamos modificaiones para que funcione con imports y exports de tipo ES6

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const path = require('path')
+//const express = require('express')
+import express from 'express'
+//const path = require('path')
+import path from 'path'
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const {port, public_path = 'public'} = options
     console.log(port)
     console.log(public_path)
@@ -20,6 +22,3 @@ const startServer = (options) => {
     })
 }
 
-module.exports = {
-    startServer
-}
